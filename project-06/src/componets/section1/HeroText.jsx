4d18b1ecdfa8fb48ebce26643e4d8be773ab5e97
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-10-29 09:44:25 +0530

    done with card section :

---
+++ project-06/src/componets/section1/HeroText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className=' p-8'>
+             <h3 className=' mb-7 text-7xl font-bold '>
+            Prospective <br /> <span className=' bg-gray-300'>Customer</span> <br /> Section
+            </h3>
+            <p className=' text-lg  font-medium text-gray-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam accusantium reiciendis debitis ad laborum assumenda.</p>
+            </div>
+  )
+}
+
+export default HeroText
